f86b1b5a95e6f1490de5205c9c7dbe3c785279e0
PZagade <90115971+Mandalorean@users.noreply.github.com>
2021-10-05 16:44:01 -0400

    Update test_triangle.py

--- test_triangle.py
+++ test_triangle.py
@@ -13,32 +13,20 @@ class TestTriangles(unittest.TestCase):
     def testEquilateralTriangles(self):
         self.assertEqual(classifyTriangle(1,1,1), 'Equilateral','1,1,1 should be equilateral')
 
-    def testEquilateralTrianglesA(self):
-        self.assertEqual(classifyTriangle(1,1,2), 'Equilateral','1,1,1 should be equilateral (False Fail)')
+    
 
     def testScaleneTriangleA(self):
         from triangle import classifyTriangle
         self.assertEqual(classifyTriangle(1,2,3), 'Scalene Triangle', '3,4,5 is a Scalene  triangle')
 
-    def testIsocelesTriangleA(self):
-        from triangle import classifyTriangle
-        self.assertEqual(classifyTriangle(2,2,3), 'Isoceles Triangle', '3,4,5 is a Isoceles triangle')
+   
 
-    def testTriangleA(self):
-        from triangle import classifyTriangle
-        self.assertEqual(classifyTriangle(222,2,3), 'Scalene Triangle', ' valid values for triangle')
+ 
 
-    def testTriangleB(self):
-        from triangle import classifyTriangle
-        self.assertEqual(classifyTriangle(22,0,3), 'Scalene Triangle', ' valid values for triangle')
+ 
 
-    def testTriangleC(self):
-        from triangle import classifyTriangle
-        self.assertEqual(classifyTriangle(22,8,0), 'Scalene Triangle', ' valid values for triangle')
 
-    def testTriangleD(self):
-        from triangle import classifyTriangle
-        self.assertEqual(classifyTriangle(-3,8,6), 'Scalene Triangle', ' valid values for triangle')
+    
 
     
 
