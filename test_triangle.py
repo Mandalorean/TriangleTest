1945bbaaaf58be4475326b9e949867f351c3c8c3
PZagade <90115971+Mandalorean@users.noreply.github.com>
2021-10-09 14:41:40 -0400

    Update test_triangle.py

--- test_triangle.py
+++ test_triangle.py
@@ -1,34 +1,45 @@
 import unittest
-from triangle import classifyTriangle
+from triangle import classify_triangle
 
 class TestTriangles(unittest.TestCase):
 
     def testScaleneRightTriangleA(self):
-        from triangle import classifyTriangle
-        self.assertEqual(classifyTriangle(3,4,5), 'Scalene Right Triangle', '3,4,5 is a Scalene Right triangle')
+        from triangle import classify_triangle
+        self.assertEqual(classify_triangle(3,4,5), 'Scalene Right Triangle', '3,4,5 is a Scalene Right triangle')
 
     def testScaleneRightTriangleB(self):
-        self.assertEqual(classifyTriangle(5,3,4), 'Scalene Right Triangle','5,3,4 is a Scalene Right triangle')
+        self.assertEqual(classify_triangle(5,3,4), 'Scalene Right Triangle','5,3,4 is a Scalene Right triangle')
 
     def testEquilateralTriangles(self):
-        self.assertEqual(classifyTriangle(1,1,1), 'Equilateral','1,1,1 should be equilateral')
+        self.assertEqual(classify_triangle(1,1,1), 'Equilateral','1,1,1 should be equilateral')
 
-    
+
 
     def testScaleneTriangleA(self):
-        from triangle import classifyTriangle
-        self.assertEqual(classifyTriangle(1,2,3), 'Scalene Triangle', '3,4,5 is a Scalene  triangle')
+        from triangle import classify_triangle
+        self.assertEqual(classify_triangle(1,2,3), 'Scalene Triangle', '3,4,5 is a Scalene  triangle')
 
-   
+    def testIsocelesTriangleA(self):
+        from triangle import classify_triangle
+        self.assertEqual(classify_triangle(2,2,3), 'Isoceles Triangle', '3,4,5 is a Isoceles triangle')
 
- 
+    def testTriangleA(self):
+        from triangle import classify_triangle
+        self.assertEqual(classify_triangle(222,2,3), 'InvalidInput', ' valid values for triangle')
 
- 
+    def testTriangleB(self):
+        from triangle import classify_triangle
+        self.assertEqual(classify_triangle(22,0,3), 'InvalidInput', ' valid values for triangle')
+
+    def testTriangleC(self):
+        from triangle import classify_triangle
+        self.assertEqual(classify_triangle(22,8,0), 'InvalidInput', ' valid values for triangle')
+
+    def testTriangleD(self):
+        from triangle import classify_triangle
+        self.assertEqual(classify_triangle(-3,8,6), 'InvalidInput', ' valid values for triangle')
 
 
-    
-
-    
 
 
 
