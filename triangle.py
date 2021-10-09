a6696e109026ebb98b8cc9d5ca6c949ca6e23928
PZagade <90115971+Mandalorean@users.noreply.github.com>
2021-10-09 14:42:19 -0400

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,36 +1,43 @@
-def classifyTriangle(a,b,c):
-    """    Your correct code goes here...  Fix the faulty logic below until the code passes all of
-     you test cases.This function returns a string with the type of triangle from three integer values    corresponding to the lengths of the three sides of the Triangle.
-    return:        If all three sides are equal, return 'Equilateral'        If exactly one pair of sides are equal, return 'Isoceles'        If no pair of  sides are equal, return 'Scalene'        If not a valid triangle, then return 'NotATriangle'        If the sum of any two sides equals the squate of the third side, then return 'Right'
-    BEWARE: there may be a bug or two in this code    """
+#check if the triangle is equilateral, isoceles, scalene or right
+def classify_triangle(side_a,side_b,side_c):
+
     # require that the input values be >= 0 and <= 200
-    if a >= 200 or b >= 200 or c >= 200:
-        return 'InvalidInput'
+    if side_a>=200 or side_b>=200 or side_c>=200:
+        return'InvalidInput'
 
-    if a <= 0 or b <= 0 or c <= 0:
-        return 'InvalidInput'
+    if side_a<=0 or side_b<=0 or side_c<=0:
+        return'InvalidInput'
 
-    if not (isinstance(a, int) and isinstance(b, int) and isinstance(c, int)):
-        return 'InvalidInput';
+    if not (isinstance(side_a,int) and isinstance(side_b,int) and isinstance(side_c,int)):
+        return'InvalidInput'
 
-    if (a >= (b + c)) and (b >= (a + c)) and (c >= (a + b)):
-        return 'NotATriangle'
+    if (side_a>=(side_b+side_c)) and (side_b>=(side_a+side_c)) and (side_c>=(side_a+side_b)):
+        return'NotATriangle'
 
-    if a == b and c == a and b == c :
-        return 'Equilateral'
+    if side_a==side_b and side_c==side_a and side_b==side_c:
+        return'Equilateral'
 
-    if ((a * a) + (b * b)) == (c * c) or ((c * c) + (b * b)) == (a * a) or ((a * a) + (c * c)) == (b * b):
-        if (a != b) and (b != c) and (a != b):
-            return 'Scalene Right Triangle'
+    if ((side_a**2) + (side_b**2)) == (side_c**2) \
+            or ((side_c**2) + (side_b**2)) == (side_a**2) \
+            or ((side_a**2) + (side_c**2)) == (side_b**2):
+
+        if side_b not in (side_a, side_c) and\
+                side_a not in (side_b, side_c) and\
+                side_c not in (side_a, side_b):
+            return'Scalene Right Triangle'
         else:
             return 'Isoceles Right Triangle'
 
-    if  (a != b) and (b != c) and (a != b):
-        return 'Scalene Triangle'
+    if side_b not in (side_a, side_c) and\
+        side_a not in (side_b, side_c) and\
+        side_c not in (side_a, side_b):
+        return'Scalene Triangle'
     else:
-            return 'Isoceles Triangle'
+        return 'Isoceles Triangle'
 
 
-if __name__ == '__main__':
+
+
+if __name__=='__main__':
     # examples of running the code
-    print('{}'.format(classifyTriangle(1,2,3)))
+    print(f'{classify_triangle(4,5,3)}')
